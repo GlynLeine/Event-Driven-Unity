e02765b47ebab0d2500ea4d5961b6aed348afc42
Arjen de Aldrey <adealdrey1@gmail.com>
2020-05-29 11:19:47 +0200

    [ADD] player script

---
+++ Event-driven-Masterclass/Assets/Class Live Code/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private List<AudioSource> footsteps;
+
+    private Vector3 _movementDirectionVector;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        _movementDirectionVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
+        sound();
+
+        transform.position += _movementDirectionVector.normalized * speed * Time.deltaTime;
+    }
+
+    private void sound()
+    {
+        if (_movementDirectionVector.magnitude > 0)
+        {
+            int randomFootstepCount = Random.Range(0, footsteps.Count);
+            //AudioSource
+
+
+        }
+    }
+}
