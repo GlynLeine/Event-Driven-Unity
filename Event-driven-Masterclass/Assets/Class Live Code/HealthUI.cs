3edd19efd7a82465b27e4049bb159d7548dd94f2
GlynLeine <g.m.leine@gmail.com>
2020-05-29 11:20:40 +0200

    health ui script

---
+++ Event-driven-Masterclass/Assets/Class Live Code/HealthUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthUI : MonoBehaviour
+{
+    Text text;
+
+    public void OnHealthChanged(float health)
+    {
+        text.text = "HP " + health;
+    }
+}
